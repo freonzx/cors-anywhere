8827eb1219f068f59e9c1502c7a0fa51bddc1c2f
Yago Theodoro <freonzx@gmail.com>
2019-02-16 20:41:35 -0200

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
-  checkRateLimit: checkRateLimit,
+  //checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
     'cookie2',
